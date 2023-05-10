9af34fb6384b5122fcbd0c5f68daaa49fc0b1c99
Emmanuel Boachie <bemanub@gmail.com>
2023-05-10 23:32:33 +0100

    #01 Populating admin page

--- dansah/devotional/admin.py
+++ dansah/devotional/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+
 from .models import Devotional
 
-admin.site.register(Devotional)
+
+@admin.register(Devotional)
+class PersonAdmin(admin.ModelAdmin):
+    search_fields = ("devotion_title__startswith",)
+    fields = ("devotion_title", "devotion_message", "devotion_date", "image")
+    list_display = ("devotion_title", "devotion_message", "devotion_date", "image")
+    list_filter = ("devotion_created_at", "devotion_monthly")
+
--- dansah/devotional/models.py
+++ dansah/devotional/models.py
@@ -1,5 +1,6 @@
 import os
 import uuid
+from datetime import date
 from random import random
 
 from django.db import models
@@ -22,11 +23,15 @@ class Devotional(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     devotion_title = models.CharField("Devotion title", max_length=255)
     devotion_message = models.CharField("Devotion message", max_length=2048)
-    devotion_date = models.DateField("Devotion date")
+    devotion_date = models.DateField("Devotion date",default=date.today())
     image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
     devotion_created_at = models.DateTimeField(auto_now_add=True)
     devotion_updated_at = models.DateTimeField(auto_now_add=True)
     devotion_monthly = models.BooleanField("Monthly devotion", default=False)
+
+    class Meta:
+        ordering = ("devotion_title", "devotion_date")
+
 
     def __unicode__(self):
         return u'%s: /n %s' % (self.devotion_title,
