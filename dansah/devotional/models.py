fdbbab09cbe4ed0f597665f0debfbd3f6c62742a
Emmanuel Boachie <bemanub@gmail.com>
2023-05-21 07:41:42 +0100

    #005 Expanding the models

--- dansah/devotional/models.py
+++ dansah/devotional/models.py
@@ -1,14 +1,14 @@
 import uuid
-from datetime import date
 
 from django.db import models
+from django.utils import timezone
 
 
 class Devotional(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     devotion_title = models.CharField("Devotion title", max_length=255)
     devotion_message = models.TextField("Devotion message", max_length=2048)
-    devotion_date = models.DateField("Devotion date", default=date.today())
+    devotion_date = models.DateField("Devotion date", default=timezone.now)
     devotion_created_at = models.DateTimeField(auto_now_add=True)
     devotion_updated_at = models.DateTimeField()
     devotion_monthly = models.BooleanField("Monthly devotion", default=False)
