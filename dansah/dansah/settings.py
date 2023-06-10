0bae160c8982a0a5d92036ef78c27588f19f8d3a
Emmanuel Boachie <bemanub@gmail.com>
2023-06-10 11:52:45 +0100

    #006 removing devotional from setting

--- dansah/dansah/settings.py
+++ dansah/dansah/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'role',
     'powerliving',
     'socialmedia',
-    'corsheaders'
+
 
 ]
 
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+
 ]
 
 ROOT_URLCONF = 'dansah.urls'
