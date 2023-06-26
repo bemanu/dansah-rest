31c06c22efd566f47939da77f552d3bdb6c9ede6
Emmanuel Boachie <bemanub@gmail.com>
2023-06-26 16:53:39 +0100

    #11801 Vercel deployment settings

--- dansah/dansah/settings.py
+++ dansah/dansah/settings.py
@@ -93,10 +93,10 @@ WSGI_APPLICATION = 'dansah.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
