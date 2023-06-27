7b0432b54db82eb7aca04005c987387e316b7826
Emmanuel Boachie <bemanub@gmail.com>
2023-06-27 20:00:59 +0100

    #20230701 Vercel deployment settings

--- dansah/dansah/settings.py
+++ dansah/dansah/settings.py
@@ -22,10 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f: SECRET_KEY = f.read().strip()
-
-
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -92,6 +89,7 @@ TEMPLATES = [
             ],
         },
     },
+
 ]
 
 WSGI_APPLICATION = 'dansah.wsgi.application'
@@ -103,13 +101,14 @@ DATABASES = {
 
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'URL':'postgresql://postgres:XN8w1Bm4A2mTfUUci1ZV@containers-us-west-129.railway.app:7643/railway',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'XN8w1Bm4A2mTfUUci1ZV',
-        'HOST': 'containers-us-west-129.railway.app',
-        'PORT': '7643',
+        'URL': os.getenv('POSTGRES_URL'),
+        'NAME': os.getenv('PGNAME'),
+        'USER': os.getenv('PGUSER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('PGHOST'),
+        'PORT': os.getenv('PGPORT'),
     }
+
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
