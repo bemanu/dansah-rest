429ca2329782f01cd63dd9bea504ad0f5474027b
Emmanuel Boachie <bemanub@gmail.com>
2023-07-17 12:52:08 +0100

    #Defining Static file path

--- dansah/settings.py
+++ dansah/settings.py
@@ -147,14 +147,16 @@ else:
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-    STATIC_URL = "static/"
+    # STATIC_URL = "static/"
     # STATIC_ROOT = os.path.join(BASE_DIR, "static")
-    # STATIC_DIR = os.path.join(BASE_DIR, "static")
+    # STATICFILES_DIRS = [
+    #     os.path.join(BASE_DIR, "static"),
+    # ]
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATIC_URL = "/staticfiles/"
 
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"),
-    ]
+    # Extra places for collectstatic to find static files.
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
     MEDIA_URL = "/media/"
     # Default primary key field type
