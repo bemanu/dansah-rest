e317ea1e4296d876ed3699ebea497febd820834b
jrferguson <1q2w3e4r5t6y>
2023-07-17 08:39:35 +0100

    test static

--- dansah/settings.py
+++ dansah/settings.py
@@ -148,7 +148,7 @@ else:
     # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
     STATIC_URL = "static/"
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
     MEDIA_URL = "/media/"
     # Default primary key field type
