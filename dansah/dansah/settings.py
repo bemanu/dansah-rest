06355098f34b06bca5ab7175d12f1b6ee548c485
Emmanuel Boachie <bemanub@gmail.com>
2023-07-26 16:19:30 +0100

    Editing the settings for deployment

--- dansah/dansah/settings.py
+++ dansah/dansah/settings.py
@@ -148,6 +148,32 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+# if USE_S3:
+#     # aws settings
+#     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+#     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+#     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+#     AWS_DEFAULT_ACL = 'public-read'
+#     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+#     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+#     # s3 static settings
+#     AWS_LOCATION = 'static'
+#     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+#     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# else:
+#     # Static files (CSS, JavaScript, Images)
+#     # https://docs.djangoproject.com/en/4.2/howto/static-files/
+#
+#     STATIC_URL = 'static/'
+#     MEDIA_URL = '/media/'
+#     # Default primary key field type
+#     # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+
+
+
+
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", 'media_root')
