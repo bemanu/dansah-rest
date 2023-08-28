d187b6480e3bf84ecbe0484099a286fc1550b5c5
Emmanuel Boachie <bemanub@gmail.com>
2023-08-28 17:59:37 +0100

    setting changes

--- dansah/build_file.sh
+++ dansah/build_file.sh
@@ -1,4 +1,8 @@
-   echo "BUILD START"
-   python3.9 -m pip install -r requirements.txt
-   python3.9 manage.py collectstatic --noinput --clear
-   echo "BUILD END"
+echo "BUILD START INSTALLING REQUIREMENTS"
+python3.9 -m pip install -r requirements.txt
+
+echo "Collect static files"
+python3.9 manage.py collectstatic
+
+
+echo "BUILD END"
